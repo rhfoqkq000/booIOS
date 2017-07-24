6bd7de4cab197b18de376aec2862ab919fb56960
rhfoqkq000 <rhfoqkq000@naver.com>
2017-07-24 20:04:57 +0900

    메뉴 section추가

---
+++ booIOS/Menu/HeaderCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  HeaderCollectionReusableView.swift
+//  booIOS
+//
+//  Created by candy on 2017. 7. 24..
+//  Copyright © 2017년 univ. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+        
+    @IBOutlet weak var headerTitle: UILabel!
+}
--- booIOS/Menu/MenuViewController.swift
+++ booIOS/Menu/MenuViewController.swift
@@ -19,13 +19,21 @@ class MenuViewController: UIViewController, UICollectionViewDelegate, UICollecti
 
     @IBOutlet weak var menuCollection: UICollectionView!
     
-    var images = ["emptIcon","profIcon", "leIcon", "siteIcon", "noticeIcon", "changeIcon", "helpIcon", "logoutIcon", "crownIcon"]
-    var titles = ["빈강", "To. 교수님", "쪽지함", "사이트", "공지사항","동이라/학회 변경", "설정/도움말", "로그아웃", "관리자로그인"]
+    let sectionTitle = ["메뉴", "설정", "로그인/로그아웃"]
+    let menuIcon = ["emptIcon","profIcon", "leIcon","siteIcon"]
+    let settingIcon = ["noticeIcon", "changeIcon", "helpIcon"]
+    let logIcon = ["logoutIcon", "crownIcon"]
+    
+    let menuTitle = ["빈강", "To. 교수님", "쪽지함", "사이트"]
+    let settingTitle = ["공지사항","동아리/학회 변경", "설정/도움말"]
+    let logTitle = ["로그아웃", "관리자로그인"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,41 +42,76 @@ class MenuViewController: UIViewController, UICollectionViewDelegate, UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count
+        switch section {
+        case 0:
+            return 4
+        case 1:
+            return 3
+        case 2:
+            return 2
+        default:
+            print("??")
+            return 0
+        }
     }
     
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        var reusableview = UICollectionReusableView()
+        if kind == UICollectionElementKindSectionHeader{
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerView", for: indexPath) as! HeaderCollectionReusableView
+            
+            headerView.headerTitle.text = sectionTitle[indexPath.section]
+            reusableview = headerView
+        }
+         return reusableview
+    }
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "menuCollection", for: indexPath) as! MenuCollectionViewCell
         
-        cell.menuImage.image = UIImage(named: images[indexPath.row])
-        cell.menuLabel.text = titles[indexPath.row]
-        
+        if indexPath.section==0 {
+            cell.menuImage.image = UIImage(named: menuIcon[indexPath.row])
+            cell.menuLabel.text = menuTitle[indexPath.row]
+        } else if(indexPath.section==1){
+            cell.menuImage.image = UIImage(named: settingIcon[indexPath.row])
+            cell.menuLabel.text = settingTitle[indexPath.row]
+        }else {
+            cell.menuImage.image = UIImage(named: logIcon[indexPath.row])
+            cell.menuLabel.text = logTitle[indexPath.row]
+        }
         return cell
     }
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let ctrls = Ctrls()
-        switch indexPath.row {
+        switch indexPath.section {
         case 0:
-            self.navigationController?.pushViewController(returnTargetCtrl(ctrls.empty),animated: true)
+            switch indexPath.row{
+            case 0:
+                self.navigationController?.pushViewController(returnTargetCtrl(ctrls.empty),animated: true)
+                break
+            case 1:
+                self.navigationController?.pushViewController(returnTargetCtrl(ctrls.prof),animated: true)
+                break
+            case 2:
+                break
+            case 3:
+                self.navigationController?.pushViewController(returnTargetCtrl(ctrls.site),animated: true)
+                break
+            default:
+                break
+            }
             break
         case 1:
-            self.navigationController?.pushViewController(returnTargetCtrl(ctrls.prof),animated: true)
-            break
+            switch  indexPath.row {
+            case 0:
+                self.navigationController?.pushViewController(returnTargetCtrl(ctrls.notice),animated: true)
+                break
+            default:
+                break
+            }
+          break
         case 2:
-            break
-        case 3:
-            self.navigationController?.pushViewController(returnTargetCtrl(ctrls.site),animated: true)
-            break
-        case 4:
-            self.navigationController?.pushViewController(returnTargetCtrl(ctrls.notice),animated: true)
-            break
-        case 5:
-            break
-        case 6:
-            break
-        case 7:
-            break
-        case 8:
             break
         default:
             print("오류에영")
